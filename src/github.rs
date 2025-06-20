0d008c9ea439c607b6f34a9521e76569ff0ae1a1
Braqzen <103777923+Braqzen@users.noreply.github.com>
2025-06-20 15:31:52 +0100

    GH tests

--- src/github.rs
+++ src/github.rs
@@ -33,6 +33,8 @@ impl Github {
     }
 
     /// Extract owner and repo from the GitHub URL
+    ///
+    /// SAFETY: assumes source does not have an extension (e.g. .git)
     pub fn split_source(source: &str) -> Result<(String, String)> {
         let parts: Vec<&str> = source
             .trim_start_matches("https://github.com/")
@@ -48,8 +50,10 @@ impl Github {
     }
 
     /// Check if a repository exists and return the default branch
+    ///
+    /// SAFETY: "default_branch" is in response
     pub async fn check_repo(&self, owner: &str, repo: &str) -> Option<String> {
-        let url = format!("https://api.github.com/repos/{owner}/{repo}");
+        let url = Self::repo_url(owner, repo);
 
         match self
             .client
@@ -78,7 +82,7 @@ impl Github {
         path: &str,
         ref_: &str,
     ) -> Result<bool> {
-        let url = format!("https://api.github.com/repos/{owner}/{repo}/contents/{path}?ref={ref_}");
+        let url = Self::file_url(owner, repo, path, ref_);
 
         // TODO: can this return a success for a file that does not exist
         let response = self
@@ -94,5 +98,96 @@ impl Github {
     /// Format a GitHub URL for a file at a given revision
     pub fn web_url(owner: &str, repo: &str, file_path: &str, revision: &str) -> String {
         format!("https://github.com/{owner}/{repo}/blob/{revision}/{file_path}")
+    }
+
+    fn repo_url(owner: &str, repo: &str) -> String {
+        format!("https://api.github.com/repos/{owner}/{repo}")
+    }
+
+    fn file_url(owner: &str, repo: &str, path: &str, ref_: &str) -> String {
+        format!("https://api.github.com/repos/{owner}/{repo}/{path}?ref={ref_}")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const OWNER: &str = "owner";
+    const REPO: &str = "repo";
+    const PATH: &str = "path";
+    const REF: &str = "ref";
+
+    mod public {
+        use super::*;
+
+        // TODO: unsure about check_repo() and file_exists() like in docker.rs
+
+        #[test]
+        fn test_sha() {
+            assert!(Github::sha("1234567890123456789012345678901234567890"));
+        }
+
+        #[test]
+        fn test_sha_invalid_length() {
+            assert!(!Github::sha("1234567890"));
+        }
+
+        #[test]
+        fn test_sha_invalid_characters() {
+            assert!(!Github::sha("123456789012345678901234567890123456789*"));
+        }
+
+        #[test]
+        fn test_format_source() {
+            assert_eq!(
+                Github::format_source("git@github.com:owner/repo.git"),
+                "https://github.com/owner/repo"
+            );
+        }
+
+        #[test]
+        fn test_split_source() -> Result<()> {
+            assert_eq!(
+                Github::split_source(&format!("https://github.com/{OWNER}/{REPO}"))?,
+                (OWNER.to_string(), REPO.to_string())
+            );
+
+            Ok(())
+        }
+
+        #[test]
+        fn test_split_source_invalid() {
+            let error = Github::split_source("https://github.com/owner");
+            assert!(error.is_err());
+        }
+
+        #[test]
+        fn test_web_url() {
+            assert_eq!(
+                Github::web_url(OWNER, REPO, PATH, REF),
+                "https://github.com/owner/repo/blob/ref/path"
+            );
+        }
+    }
+
+    mod private {
+        use super::*;
+
+        #[test]
+        fn test_repo_url() {
+            assert_eq!(
+                Github::repo_url(OWNER, REPO),
+                "https://api.github.com/repos/owner/repo"
+            );
+        }
+
+        #[test]
+        fn test_file_url() {
+            assert_eq!(
+                Github::file_url(OWNER, REPO, PATH, REF),
+                "https://api.github.com/repos/owner/repo/path?ref=ref"
+            );
+        }
     }
 }
